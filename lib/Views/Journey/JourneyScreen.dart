df8769f1957017f66929c1a63d15fe8453cf76fa
Mahmoud <ite.mahmoudabusalou@gmail.com>
2022-05-10 16:36:47 +0300

    Initial Project

---
+++ lib/Views/Journey/JourneyScreen.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:flutter/material.dart';
+
+class Journey extends StatelessWidget {
+  const Journey({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,54 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:tourist_app/shared/network/local/cachehelper.dart';
+import 'package:tourist_app/shared/network/remote/dio_helper.dart';
+
+import 'shared/bloc_observer.dart';
+
+void main() async {
+  HttpOverrides.global = MyHttpOverrides();
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = MyBlocObserver();
+  DioHelper.init();
+  await CacheHelper.init();
   runApp(const MyApp());
 }
-
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
 
-        primarySwatch: Colors.blue,
-      ),
-      home:Container(),
-    );
+    return ScreenUtilInit(
+        designSize: Size(1080, 2280),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (_) {
+          return MultiBlocProvider(
+              providers: [
+
+              ],
+              child:MaterialApp(
+
+                home: Container(),
+              )
+
+
+              );
+        });
   }
 }
-
